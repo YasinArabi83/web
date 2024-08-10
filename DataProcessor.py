87473369b921652e4c9428e9a48f395b896b2f6b
Yasin Arabi <yasinarabi1383@gmail.com>
2024-08-11 02:57:32 +0330

    feat:Add DataProcessor class to convert raw data to database format

---
+++ DataProcessor.py
@@ -0,0 +1,22 @@
+class DataProcessor:
+    def __init__(self, pages: list):
+        self.pages = pages
+
+    def data_processor(self):
+        car_ads = []
+        try_pars = lambda is_number: int(is_number) if is_number.isdecimal() else 0
+        format_modifier = lambda mileage: mileage.replace(',', '').replace(' km', '')
+        for page in self.pages:
+            for ad in page:
+                if ad["type"] == "ad":
+                    car_ad = {
+                        "title": ad["detail"]["title"],
+                        "year": int(ad["detail"]["year"]),
+                        "mileage": try_pars(format_modifier(ad["detail"]["mileage"])),
+                        "location": ad["detail"]["location"],
+                        "body_color": ad["detail"]["body_color"],
+                        "price": ad["price"]["price"],
+                    }
+                    car_ads.append(car_ad)
+
+        return car_ads
