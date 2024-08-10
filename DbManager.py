b9745fc25e7485cb383ea1b3d13f08f9de774e13
Yasin Arabi <yasinarabi1383@gmail.com>
2024-08-10 17:28:04 +0330

    feat: Add create_table method for asynchronous table creation

--- DbManager.py
+++ DbManager.py
@@ -1,7 +1,13 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 
+from model.base import Base
+
 
 class AsyncDbManager:
     def __init__(self, db_url):
         self.engine = create_async_engine(db_url, echo=True)
         self.async_session = async_sessionmaker(bind=self.engine, class_=AsyncSession)
+
+    async def create_table(self):
+        async with self.engine.begin() as conn:
+            await conn.run_sync(Base.metadata.create_all)
